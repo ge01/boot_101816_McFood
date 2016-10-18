d299c91c9c8197b5af499fd9e07ca13dba73d43f
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-10-18 13:53:49 -0500

    chinese food working

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ console.log(choice.toLowerCase());
 // The user chose Mexican food
 if (choice === "mexican" ) {
   console.log(choice);
+
+  // Decide on Tacos or Enhiladas
   var mexicanChoice = prompt("Do you want Tacos or Enchiladas");
   mexicanChoice.toLowerCase();
 
-  // Decide on Tacos or Enchilads
+  // Response to Tacos or Enchilads
   if (mexicanChoice === "tacos") {
     prompt("Orale! Tacos de bistec is the way to go.");
   }
@@ -30,12 +32,33 @@ if (choice === "mexican" ) {
   }
 }
 
+// The user chose Chinese food
 else if (choice === "chinese") {
   console.log(choice);
+
+  // Decide on Wonton or Tofu
+  var chineseChoice = prompt("Do you want Wonton or Tofu");
+  chineseChoice.toLowerCase();
+
+  // Response to Wonton or Tofu
+  if (chineseChoice === "wonton") {
+    prompt("Ni Hao! Wonton-amera it is.");
+  }
+  else if (chineseChoice === "tofu") {
+    prompt("Bùcuò! I pitty the tofu...")
+  }
+  else {
+    prompt("Hmm...don't think you chose Wonton or Tofu?");
+  }
 }
+
+// The user does not know
 else if (choice === "idunno") {
   console.log(choice);
+  prompt("You're going to get a cold cut trio from Subway");
 }
+
+// The user didn't enter a valid choice
 else
 {
   console.log("You didn't select one of the three options.")
